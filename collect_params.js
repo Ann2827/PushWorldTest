cfa122bb8be69e96471544769c55177c60bbbe55
Быстрова Анна <a.bystrova@admitad.pro>
2019-05-05 15:37:03 +0300

    Create repository

---
+++ collect_params.js
@@ -0,0 +1,16 @@
+function Collect_Params(){
+  this._url = $("#basic-url").val();
+  this._headSelect = $("#heading").val();
+  if ($("#img1").val()) {
+    this._optionImg = "option1";
+    this._img = $("#img1").val()
+  } else if ($("#img2").val()) {
+    this._optionImg = "option2";
+    this._img = $("#img2").val()
+  } else {alert("Error")}
+};
+$("button").on("click", function(){
+  var Params = new Collect_Params();
+  var Str = JSON.stringify(Params);
+  alert(Str);
+})
