686f2e1b9d2b5a9d8c34b48c16f066e30cf0379c
Быстрова Анна <a.bystrova@admitad.pro>
2019-05-21 17:45:03 +0300

    123

--- public/firebase_subscribe.js
+++ public/firebase_subscribe.js
@@ -1,243 +1,267 @@
 const KEY = "AAAASqJfht4:APA91bFJaIKpQgX-ZkZlgk9hKf122NCy7H17_KLJU-MnStIIAQzAcg5LBXlCF-s0EjdLMT1Uym44xqURZvS31k7WUW6nf1faCoW6G62wuR8EsCzIneITn2j3ZijitOXQaHgfIHL9NpJV";
 
 firebase.initializeApp({
-    messagingSenderId: '320551749342'
+  messagingSenderId: '320551749342'
 });
 
 // Проверка поддерживаемости уведомлений
 if ('Notification' in window &&
-    'serviceWorker' in navigator &&
-    'localStorage' in window &&
-    'fetch' in window &&
-    'postMessage' in window) {
-    var messaging = firebase.messaging();
+  'serviceWorker' in navigator &&
+  'localStorage' in window &&
+  'fetch' in window &&
+  'postMessage' in window) {
+  var messaging = firebase.messaging();
 
-    // Проверка подписки
-    if (Notification.permission === 'granted') {
-        //subscribe();
-    }
+  // Проверка подписки
+  if (Notification.permission === 'granted') {
+    //subscribe();
+  }
 
-    // Подписать пользователя
-    $("#form1").on("submit", function (event) {
-        event.preventDefault();
-        //регистрация вручную, мб будет не нужно на хостинге
-        navigator.serviceWorker.register('firebase-messaging-sw.js').then(function (registration) {
-            messaging.useServiceWorker(registration);
-            subscribe();
-        })
-    });
+  // Подписать пользователя
+  $("#form1").on("submit", function (event) {
+    event.preventDefault();
+    //регистрация вручную, мб будет не нужно на хостинге
+    navigator.serviceWorker.register('firebase-messaging-sw.js').then(function (registration) {
+      messaging.useServiceWorker(registration);
+      subscribe();
+    })
+  });
 
-    //Удалить токен
-    $("#deleet_token").on("click", function () {
-        messaging.getToken().then(function (currentToken) {
-                messaging.deleteToken(currentToken).then(function () {
-                        console.log('Удаление токена.');
-                        setTokenSentToServer(false);
-                        location.reload();
-                        //$(".alert").addClass("d-none");
-                    })
-                    .catch(function (error) {
-                        showError('Unable to delete token', error);
-                    });
-            })
-            .catch(function (error) {
-                showError('Error retrieving Instance ID token', error);
-            });
-    });
+  //Удалить токен
+  $("#deleet_token").on("click", function () {
+    messaging.getToken().then(function (currentToken) {
+        messaging.deleteToken(currentToken).then(function () {
+            console.log('Удаление токена.');
+            setTokenSentToServer(false);
+            location.reload();
+            //$(".alert").addClass("d-none");
+          })
+          .catch(function (error) {
+            showError('Unable to delete token', error);
+          });
+      })
+      .catch(function (error) {
+        showError('Error retrieving Instance ID token', error);
+      });
+  });
 
-    //Для показа пушей
-    messaging.onMessage(function (payload) {
-        console.log('Message received', payload);
+  //Для показа пушей
+  messaging.onMessage(function (payload) {
+    console.log('Message received', payload);
 
-        // register fake ServiceWorker for show notification on mobile devices
-        navigator.serviceWorker.register('firebase-messaging-sw.js');
-        Notification.requestPermission(function (permission) {
-            if (permission === 'granted') {
-                navigator.serviceWorker.ready.then(function (registration) {
-                    // Copy data object to get parameters in the click handler
-                    payload.data.data = JSON.parse(JSON.stringify(payload.data));
+    // register fake ServiceWorker for show notification on mobile devices
+    navigator.serviceWorker.register('firebase-messaging-sw.js');
+    Notification.requestPermission(function (permission) {
+      if (permission === 'granted') {
+        navigator.serviceWorker.ready.then(function (registration) {
+          // Copy data object to get parameters in the click handler
+          payload.data.data = JSON.parse(JSON.stringify(payload.data));
 
-                    registration.showNotification(payload.data.title, payload.data);
-                }).catch(function (error) {
-                    // registration failed :(
-                    showError('ServiceWorker registration failed', error);
-                });
-            }
+          registration.showNotification(payload.data.title, payload.data);
+        }).catch(function (error) {
+          // registration failed :(
+          showError('ServiceWorker registration failed', error);
         });
+      }
     });
+  });
 } else {
-    alert("Browser don`t support Notification in window");
+  alert("Browser don`t support Notification in window");
 
-    if (!('Notification' in window)) {
-        showError('Notification not supported');
-    } else if (!('serviceWorker' in navigator)) {
-        showError('ServiceWorker not supported');
-    } else if (!('localStorage' in window)) {
-        showError('LocalStorage not supported');
-    } else if (!('fetch' in window)) {
-        showError('fetch not supported');
-    } else if (!('postMessage' in window)) {
-        showError('postMessage not supported');
-    }
+  if (!('Notification' in window)) {
+    showError('Notification not supported');
+  } else if (!('serviceWorker' in navigator)) {
+    showError('ServiceWorker not supported');
+  } else if (!('localStorage' in window)) {
+    showError('LocalStorage not supported');
+  } else if (!('fetch' in window)) {
+    showError('fetch not supported');
+  } else if (!('postMessage' in window)) {
+    showError('postMessage not supported');
+  }
 
-    console.warn('This browser does not support desktop notification.');
-    console.log('Is HTTPS', window.location.protocol === 'https:');
-    console.log('Support Notification', 'Notification' in window);
-    console.log('Support ServiceWorker', 'serviceWorker' in navigator);
-    console.log('Support LocalStorage', 'localStorage' in window);
-    console.log('Support fetch', 'fetch' in window);
-    console.log('Support postMessage', 'postMessage' in window);
+  console.warn('This browser does not support desktop notification.');
+  console.log('Is HTTPS', window.location.protocol === 'https:');
+  console.log('Support Notification', 'Notification' in window);
+  console.log('Support ServiceWorker', 'serviceWorker' in navigator);
+  console.log('Support LocalStorage', 'localStorage' in window);
+  console.log('Support fetch', 'fetch' in window);
+  console.log('Support postMessage', 'postMessage' in window);
 }
 
 //Подписать
 function subscribe() {
-    // Запрос разрешения на получение уведомлений
-    messaging.requestPermission().then(function () {
-        // Получаем ID устройства
+  // Запрос разрешения на получение уведомлений
+  messaging.requestPermission().then(function () {
+    // Получаем ID устройства
 
-        messaging.getToken().then(function (currentToken) {
-            console.log(currentToken);
-            if (currentToken) {
-                sendTokenToServer(currentToken);
-                $(".alert").removeClass("d-none");
-            } else {
-                console.warn('Не удалось получить токен.');
-                setTokenSentToServer(false);
-            }
-        }).catch(function (err) {
-            console.warn('При получении токена произошла ошибка.', err);
-            setTokenSentToServer(false);
-        });
+    messaging.getToken().then(function (currentToken) {
+      console.log(currentToken);
+      if (currentToken) {
+        sendTokenToServer(currentToken);
+        $(".alert").removeClass("d-none");
+      } else {
+        console.warn('Не удалось получить токен.');
+        setTokenSentToServer(false);
+      }
     }).catch(function (err) {
-        console.warn('Не удалось получить разрешение на показ уведомлений.', err);
+      console.warn('При получении токена произошла ошибка.', err);
+      setTokenSentToServer(false);
     });
+  }).catch(function (err) {
+    console.warn('Не удалось получить разрешение на показ уведомлений.', err);
+  });
 }
 
 // Отправка ID на сервер
 function sendTokenToServer(currentToken) {
-    if (!isTokenSentToServer(currentToken)) {
-        console.log('Отправка токена на сервер...');
-        setTokenSentToServer(currentToken);
-        ShowToken(currentToken);
-    } else {
-        console.log('Токен уже отправлен на сервер.');
-        ShowToken(currentToken);
-    }
+  if (!isTokenSentToServer(currentToken)) {
+    console.log('Отправка токена на сервер...');
+    setTokenSentToServer(currentToken);
+    ShowToken(currentToken);
+  } else {
+    console.log('Токен уже отправлен на сервер.');
+    ShowToken(currentToken);
+  }
 }
 
 //Отобразить на странице токен
 function ShowToken(currentToken) {
-    $(".alert").alert("show");
-    $(".alert p").html("<b>Токен:</b> " + currentToken);
+  $(".alert").alert("show");
+  $(".alert p").html("<b>Токен:</b> " + currentToken);
 }
 
 // localStorage, если уже подписан
 function isTokenSentToServer(currentToken) {
-    return window.localStorage.getItem('sentFirebaseMessagingToken') == currentToken;
+  return window.localStorage.getItem('sentFirebaseMessagingToken') == currentToken;
 }
 
 //Отправка на сервер
 function setTokenSentToServer(currentToken) {
-    window.localStorage.setItem(
-        'sentFirebaseMessagingToken',
-        currentToken ? currentToken : ''
-    );
+  window.localStorage.setItem(
+    'sentFirebaseMessagingToken',
+    currentToken ? currentToken : ''
+  );
 }
 
 $(function () {
-    $("#form2").on("submit", function (event) {
-        event.preventDefault();
-        //var Params = Collect_Params();
+  $("#form2").on("submit", function (event) {
+    event.preventDefault();
+    //var Params = Collect_Params();
 
-        //alert для проверки
-        //var Str = JSON.stringify(Params);
-        //alert(Str);
+    //alert для проверки
+    //var Str = JSON.stringify(Params);
+    //alert(Str);
 
-        sendNotification();
-    });
+    sendNotification();
+  });
 });
 
 function showError(error, error_data) {
-    if (typeof error_data !== "undefined") {
-        console.error(error, error_data);
-    } else {
-        console.error(error);
-    }
+  if (typeof error_data !== "undefined") {
+    console.error(error, error_data);
+  } else {
+    console.error(error);
+  }
 }
 
 function sendNotification() {
 
-    console.log('Send notification');
+  console.log('Send notification');
 
-    // hide last notification data
+  // hide last notification data
 
-    messaging.getToken().then(function (currentToken) {
-            fetch('https://fcm.googleapis.com/fcm/send', {
-                method: 'POST',
-                headers: {
-                    'Authorization': 'key=' + KEY,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    // Firebase loses 'image' from the notification.
-                    // And you must see this: https://github.com/firebase/quickstart-js/issues/71
-                    data: {
-                        click_action: $("#basic-url").val(),
-                        title: $("#title").val(),
-                        body: $("#message").val(),
-                        icon: $("#url-icon").val(),
-                        image: $("#img2").val(),
-                        color: $("#p1-color").val(),
-                        sound: $("#p2-sound").val(),
-                        tag: $("#p3-tag").val(),
-                        body_loc_key: $("#p4-body_loc_key").val(),
-                        body_loc_args: [
+  messaging.getToken().then(function (currentToken) {
+      fetch('https://fcm.googleapis.com/fcm/send', {
+        method: 'POST',
+        headers: {
+          'Authorization': 'key=' + KEY,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          // Firebase loses 'image' from the notification.
+          // And you must see this: https://github.com/firebase/quickstart-js/issues/71
+          data: {
+            action1_title: "Sale",
+            action1_url: "https://www.planeta-sport.ru/catalog/sale/s",
+            action1_icon: null,
+            click_action: $("#basic-url").val(),
+            title: $("#title").val(),
+            body: $("#message").val(),
+            icon: $("#url-icon").val(),
+            image: $("#img2").val(),
+            color: $("#p1-color").val(),
+            sound: $("#p2-sound").val(),
+            tag: $("#p3-tag").val(),
+            body_loc_key: $("#p4-body_loc_key").val(),
+            body_loc_args: [
             $("#p5-body_loc_args").val(),
           ],
-                        title_loc_key: $("#p6-title_loc_key").val(),
-                        title_loc_args: [
+            title_loc_key: $("#p6-title_loc_key").val(),
+            title_loc_args: [
             $("#p7-title_loc_args").val()
           ],
-                        action: "123123",
-                        options: {
-                            actions: [
-                                {
-                                    action: "123",
-                                    title: "123",
-                                    icon: "https://sportiv.ru/f/product/big_junior_set_sin1.jpg"
+            action: "123123",
+            options: {
+              actions: [
+                {
+                  action: "123",
+                  title: "123",
+                  icon: "https://sportiv.ru/f/product/big_junior_set_sin1.jpg"
                                 }
                             ]
-                        }
-                    },
-                    to: currentToken
-                })
-            }).then(function (response) {
-                return response.json();
-            }).then(function (json) {
-                console.log('Response', json);
-
-                if (json.success === 1) {
-                    console.log('Успех', json.results[0]);
-                } else {
-                    console.log('Ошибка', json.results[0]);
-                }
-            }).catch(function (error) {
-                showError(error);
-            });
+            }
+          },
+          to: currentToken
         })
-        .catch(function (error) {
-            showError('Error retrieving Instance ID token', error);
-        });
+      }).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        console.log('Response', json);
+
+        if (json.success === 1) {
+          console.log('Успех', json.results[0]);
+        } else {
+          console.log('Ошибка', json.results[0]);
+        }
+      }).catch(function (error) {
+        showError(error);
+      });
+    })
+    .catch(function (error) {
+      showError('Error retrieving Instance ID token', error);
+    });
 }
 
 function Collect_Params() {
-    var Params = {
-        click_action: $("#basic-url").val(),
-        title: $("#title").val(),
-        body: $("#message").val(),
-        icon: $("#url-icon").val(),
-        image: $("#img2").val(),
-        color: "#c31cd9"
-    };
-    return Params;
+  var Params = {
+    click_action: $("#basic-url").val(),
+    title: $("#title").val(),
+    body: $("#message").val(),
+    icon: $("#url-icon").val(),
+    image: $("#img2").val(),
+    color: "#c31cd9"
+  };
+  return Params;
 };
+//---------------------------
+/*{
+  "send_after": "3600",
+  "multicast": {
+    "title": "{:category_title}",
+    "text": "Одежда, обувь и снаряжение для спортсменов и путешественников.",
+    "life_time": "21600",
+    "duration": "60",
+    "action1_title": "Sale",
+    "action1_url": "https://www.planeta-sport.ru/catalog/sale/?utm_source=push-notification&utm_medium=push-world&utm_campaign=push1",
+    "action1_icon": null,
+    "action2_title": "Акции",
+    "action2_url": "https://www.planeta-sport.ru/catalog/promo/?utm_source=push-notification&utm_medium=push-world&utm_campaign=push1",
+    "action2_icon": null,
+    "abandoned_cart_timeout": "3600",
+    "abandoned_cart_url": "{:abandoned_url}?utm_source=push-notification&utm_medium=push-world&utm_campaign=push1",
+    "abandoned_cart_enabled": 1,
+    "url": "{:abandoned_url}?utm_source=push-notification&utm_medium=push-world&utm_campaign=push1",
+    "type": 6
+  }
+}*/
